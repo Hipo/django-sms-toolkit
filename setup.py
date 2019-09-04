676fe09eb4d4bce1994d3f33cf1b993821e75e5f
Ali <alicertel@gmail.com>
2019-09-04 16:54:03 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-sms-toolkit',
+    version='0.1.0',
+    packages=['django_sms_toolkit'],
+    include_package_data=True,
+    license='Apache-2.0',
+    description="It's a Django module to send and store SMS with Twilio.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='hipo',
+    author_email='pypi@hipolabs.com',
+    url='https://github.com/Hipo/django-sms-toolkit',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
