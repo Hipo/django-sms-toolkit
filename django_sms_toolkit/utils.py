24cff695280a7cb0eb6da3662aed9dc94d5782c4
Ozan Dogrultan <ozan@hipo.biz>
2019-11-01 18:09:45 +0300

    Get DEFAULT_FROM_NUMBER from Django settings
    Co-authored-by: Fergal Walsh <fergalwalsh@gmail.com>

--- django_sms_toolkit/utils.py
+++ django_sms_toolkit/utils.py
@@ -1,5 +1,10 @@
+from django.conf import settings
+
 from .tasks import send_sms as send_sms_task
 
 
-def send_sms(from_number, to_number, body, recipient_id=None):
+def send_sms(to_number, body, recipient_id=None, from_number=None):
+    if from_number is None:
+        from_number = settings.DJANGO_SMS_TOOLKIT_SETTINGS["DEFAULT_FROM_NUMBER"]
+
     return send_sms_task(from_number, to_number, body, recipient_id=None)
