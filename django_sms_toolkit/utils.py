af412339fa3bbbc9ed255d9243a60bc63f629f87
Ozan Dogrultan <ozan@hipo.biz>
2019-11-01 19:07:19 +0300

    Get DJANGO_SMS_TOOLKIT_SETTINGS from settings.py

--- django_sms_toolkit/utils.py
+++ django_sms_toolkit/utils.py
@@ -1,10 +1,9 @@
-from django.conf import settings
-
+from .settings import DJANGO_SMS_TOOLKIT_SETTINGS
 from .tasks import send_sms as send_sms_task
 
 
 def send_sms(to_number, body, recipient_id=None, from_number=None):
     if from_number is None:
-        from_number = settings.DJANGO_SMS_TOOLKIT_SETTINGS["DEFAULT_FROM_NUMBER"]
+        from_number = DJANGO_SMS_TOOLKIT_SETTINGS["DEFAULT_FROM_NUMBER"]
 
     return send_sms_task(from_number, to_number, body, recipient_id=None)
